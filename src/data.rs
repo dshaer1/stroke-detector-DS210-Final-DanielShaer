a9dcca9ba2d5d656dbccfd3ca067c1c6eeffef26
dshaer1 <dshaer@bu.edu>
2025-05-03 15:24:22 +0000

    Implemented load_data function to read patient records from CSV and uploaded CSV

--- src/data.rs
+++ src/data.rs
@@ -1,3 +1,6 @@
+use csv::ReaderBuilder;
+use std::error::Error;
+
 #[derive(Debug, Deserialize)]
 pub struct Patient {
     pub age: f32,
@@ -7,4 +10,19 @@ pub struct Patient {
     pub avg_glucose_level: f32,
     pub bmi: f32,
     pub stroke: u8,
+}
+
+pub fn load_data(path: &str) -> Result<Vec<Patient>, Box<dyn Error>> {
+    let mut rdr = ReaderBuilder::new()
+        .has_headers(true)
+        .from_path(path)?;
+
+    let mut patients = Vec::new();
+
+    for result in rdr.deserialize() {
+        let record: Patient = result?;
+        patients.push(record);
+    }
+    
+    Ok(patients)
 }
